14feb63fe2930ad0c269807a388dd810501f3a05
Roger Roelofs <roger.roelofs@bonterratech.com>
2023-10-12 17:01:41 -0400

    hook up chatgpt to the UI

--- lib/doc_chat_web/live/search_live.ex
+++ lib/doc_chat_web/live/search_live.ex
@@ -2,17 +2,46 @@ defmodule DocChatWeb.SearchLive do
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, search_activated: false, question: nil)}
+    {:ok, assign(socket, search_activated: false, question: nil, messages: [])}
   end
 
   def handle_event("submit_question", %{"question" => question}, socket) do
+    Task.async(fn ->
+      DocChat.Articles.ask(question)
+    end)
     {:noreply, assign(socket, search_activated: true, question: question)}
   end
 
+  def handle_info({ref, result}, socket) do
+    Process.demonitor(ref, [:flush])
+    {:ok, chain} = result
+    messages = format_messages(chain.messages)
+    IO.inspect messages
+    {:noreply,
+      assign(socket,
+        search_activated: false,
+        question: "",
+        chain: chain,
+        messages: messages)}
+  end
+
+  defp format_messages(messages) do
+    Enum.filter(messages, fn(msg) ->
+      IO.inspect(msg)
+      msg.content != nil && Enum.member?([:user, :assistant], msg.role)
+    end)
+    |> Enum.map(fn(msg) ->
+      %{
+        content: msg.content,
+        role: msg.role
+      }
+    end)
+  end
+
   def render(assigns) do
-    ~L"""
+    ~H"""
     <div class="flex h-screen flex-col">
-      
+
       <!-- Display submitted question at the top -->
       <%= if @search_activated do %>
       <div class="text-center w-full mt-4">
@@ -28,37 +57,37 @@ defmodule DocChatWeb.SearchLive do
 
           <!-- Search box -->
           <form phx-submit="submit_question" class="flex border rounded overflow-hidden w-1/2 mx-auto">
-            <input type="text" 
-            name="question"
-            placeholder="Enter your question..." 
-            class="p-2 flex-grow outline-none" 
-            phx-input="search" 
-            value="<%= @question %>" />
+            <input type="text"
+              name="question"
+              placeholder="Enter your question..."
+              class="p-2 flex-grow outline-none"
+              phx-input="search"
+              value={@question} />
             <button type="submit" class="p-2 bg-blue-500 text-white flex items-center justify-center w-10">
               <i class="fas fa-paper-plane"></i>
             </button>
           </form>
         </div>
       </div>
-    
+
       <!-- Sidebar for history and chat area, only when search is activated -->
-      <%= if @search_activated do %>
       <div class="flex w-full mt-10">
         <!-- Sidebar for history -->
         <div class="w-1/4 bg-gray-200 p-4 overflow-y-auto">
           <h2 class="text-xl font-bold mb-4">History</h2>
           <!-- History items here -->
         </div>
-    
+
         <!-- Main chat area -->
         <div class="w-3/4 flex flex-col">
           <div class="flex-grow overflow-y-auto p-4">
-            <!-- Chat messages go here -->
+            <%= Enum.map(@messages, fn(msg) -> %>
+              <p><%= msg.content %></p>
+            <% end) %>
           </div>
         </div>
       </div>
-      <% end %>
     </div>
     """
-  end 
+  end
 end
